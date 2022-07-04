020ecec8e7f2501f3b1994b9bffcc1f031b3d332
emi~ <esauvisky@gmail.com>
2022-07-04 07:08:09 -0300

    scan 13 points for mesh

--- Firmware/variants/1_75mm_MK3S-EINSy10a-E3Dv6full.h
+++ Firmware/variants/1_75mm_MK3S-EINSy10a-E3Dv6full.h
@@ -441,8 +441,8 @@
 #define MESH_MAX_Y 210
 
 // Mesh upsample definition
-#define MESH_NUM_X_POINTS 7
-#define MESH_NUM_Y_POINTS 7
+#define MESH_NUM_X_POINTS 13
+#define MESH_NUM_Y_POINTS 13
 // Mesh measure definition
 #define MESH_MEAS_NUM_X_POINTS 3
 #define MESH_MEAS_NUM_Y_POINTS 3
